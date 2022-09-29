358e1f15f81916b5f0328c2ce46172f46a81ed00
shashmahawar <shshnkmhwr@gmail.com>
2022-09-29 17:05:50 +0530

    Members and Messages
    1. Added Message Edit and Delete Logs
    2. Added Member Join and Leave messages

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import os
+import datetime, os
 from dotenv import load_dotenv
 import utils
 
@@ -10,6 +10,55 @@ client = commands.Bot(command_prefix='!', intents=intents, activity=discord.Acti
 @client.event
 async def on_ready():
     print('Bot Deployed!')
+
+@client.event
+async def on_member_join(member):
+    if member.guild.id == 871982588422656031:
+        channel = client.get_channel(871983126488948767)
+        bot_commands = client.get_channel(872364635637035028)
+        await channel.send(f'Hey {member.mention}, welcome to **{member.guild.name}**! Please go to {bot_commands.mention} and use `?set <kerberos>` to get your roles!')
+
+@client.event
+async def on_member_remove(member):
+    if member.guild.id == 871982588422656031:
+        channel = client.get_channel(871983126488948767)
+        await channel.send(f'**{member.name}#{member.discriminator}** just left the server')
+
+@client.event
+async def on_message_delete(message):
+    if message.author.bot:
+        return
+    if message.guild.id == 871982588422656031:
+        async for entry in message.guild.audit_logs(limit=1,action=discord.AuditLogAction.message_delete):
+            deleter = entry.user
+        if deleter.bot:
+            return
+        channel = client.get_channel(1025001667734818916)
+        embed = discord.Embed( 
+            description=f"**Message sent by {message.author.mention} deleted in {message.channel.mention}** \n {message.content}", 
+            color=0xFE4712,
+            timestamp=datetime.datetime.now()
+        )
+        embed.set_author(name=f"{deleter.name}#{deleter.discriminator}", icon_url=deleter.avatar)
+        embed.set_footer(text=f"Author: {message.author.id} | Message ID: {message.id}")
+        await channel.send(embed=embed)
+
+@client.event
+async def on_message_edit(before, after):
+    if before.author.bot:
+        return
+    if before.guild.id == 871982588422656031:
+        channel = client.get_channel(1025001667734818916)
+        embed = discord.Embed(
+            description=f"**Message edited in {before.channel.mention}** [Jump to Message]({before.jump_url})",
+            color=0x3480D5,
+            timestamp=datetime.datetime.now()
+        )
+        embed.add_field(name="Before", value=before.content, inline=False)
+        embed.add_field(name="After", value=after.content, inline=False)
+        embed.set_author(name=f"{before.author.name}#{before.author.discriminator}", icon_url=before.author.avatar)
+        embed.set_footer(text=f"User ID: {before.author.id}")
+        await channel.send(embed=embed)
 
 @client.command()
 async def ping(ctx):
