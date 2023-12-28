59451287abe410aa5a086c675b451a4eae40ea08
shashmahawar <shshnkmhwr@gmail.com>
2023-12-28 15:00:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -84,12 +84,12 @@ def get_courses(kerberos):
     courses = []
     for course in course_lists:
         if kerberos in course_lists[course]:
-            if course.startswith('2301'):
+            if course.startswith('2302'):
                 courses.append(course.split('-')[-1])
     return courses
 
 def get_course_count(code):
-    code = "2301-" + code.upper()
+    code = "2302-" + code.upper()
     with open("datafiles/course_lists.json", "r") as f:
         course_lists = json.load(f)
     if code not in course_lists:
